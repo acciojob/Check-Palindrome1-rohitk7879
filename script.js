972abd16b9cce6c3463cc68af720b51e3f761b17
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-20 13:29:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.toLowerCase().trim();
+	let i =0;
+	let j = str.length-1;
 
+	while(i <= j){
+		let start = str.charAt(i);
+		let end = str.charAt(j);
+		
+		if(start == ' '){
+			i++;
+		}else if( end == ' '){
+			j--;
+		}
+		else if(start == end){
+			i++;
+			j--;
+		}else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
